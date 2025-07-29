2a4be666b559cb3f58b1f6276c34ed5dffc3a62f
XChrisZero <vdevendetal5l@gmail.com>
2025-07-28 21:09:50 -0300

    Aprendendo DEF

---
+++ aulas/Intermediario/aula65.py
@@ -0,0 +1,11 @@
+'''
+ A Função `def` em Python: Definindo Funções Personalizadas
+
+Em Python, a palavra-chave **`def`** é usada para **definir funções**. Funções são blocos de código organizados e reutilizáveis que realizam uma tarefa específica. Definir suas próprias funções é um dos conceitos mais fundamentais e poderosos na programação, pois permite:
+
+- **Reusabilidade de Código:** Escreva um pedaço de código uma vez e use-o várias vezes em diferentes partes do seu programa, ou até mesmo em programas diferentes.
+    
+- **Organização e Modularidade:** Divida um programa grande em partes menores e mais gerenciáveis, tornando o código mais fácil de entender, depurar e manter.
+    
+- **Abstração:** Esconda a complexidade de uma tarefa por trás de um nome de função simples, permitindo que você se concentre no "o quê" a função faz, em vez do "como" ela faz.
+'''
