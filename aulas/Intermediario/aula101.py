12c574b00456517dc09eb18a64f14427d93a5cb8
XChrisZero <vdevendetal5l@gmail.com>
2025-08-28 20:29:45 -0300

    arrumando bug

--- aulas/Intermediario/aula101.py
+++ aulas/Intermediario/aula101.py
@@ -1,8 +1,9 @@
 # Exercício - Adiando execução de funções
-from exercicios import criar_funcao, soma, multiplica
+from exercicios import criar_funcao, soma, multiplica # Importando do __init__.py
 
-soma_com_cinco = criar_funcao(soma, 5)
-multiplica_por_dez = criar_funcao(multiplica, 10)
+
+soma_com_cinco = criar_funcao(soma, 5) # Adiando a execução da função soma
+multiplica_por_dez = criar_funcao(multiplica, 10)# Adiando a execução da função multiplica
 
 print(soma_com_cinco(10))
 print(multiplica_por_dez(10))
