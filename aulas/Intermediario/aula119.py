0368f598e30a4587249cbc0e024f3527ed2aaf0e
XChrisZero <vdevendetal5l@gmail.com>
2025-09-12 21:49:21 -0300

    edicoes para melhora do codigo

--- aulas/Intermediario/aula119.py
+++ aulas/Intermediario/aula119.py
@@ -1,46 +1,51 @@
 # Exercício - Lista de tarefas com desfazer e refazer
-from exercicios.exercicio_119 import listar, desfazer, refazer, adicionar, tarefas, tarefaz_refazer # Importando as funções do outro arquivo
+from exercicios.exercicio_119 import listar, desfazer, refazer, adicionar, ler, salvar, sair, tarefas, tarefas_refazer
 import os
+import json
+
+# Caminho do arquivo JSON para salvar as tarefas
+CAMINHO_ARQUIVO = 'aula119.json'
+tarefas = ler([], CAMINHO_ARQUIVO) # Lê as tarefas do arquivo JSON
+tarefas_refazer = [] # Lista para armazenar as tarefas desfeitas
 
 print ( 'Lista de tarefas!' )
-
 while True: # Loop infinito para o usuário adicionar tarefas
 
     print ( 'Comandos: \r\n[L]istar, [D]esfazer ou [R]efazer [S]air ', end= '\n \r-------------------------------\n \r' )
 
-    tarefa = input ( 'Digite a inicial de um dos comandos acima ou uma tarefa: ' ) 
-    tarefa = tarefa.upper()
+    tarefa = input ( 'Digite um dos comandos acima ou uma tarefa: ' ) 
 
-    if tarefa == 'L': # Listar as tarefas
-        listar(tarefas) 
-        continue # Volta para o início do loop
+    comandos = {
+        'listar': lambda: listar(tarefas),
+        'desfazer': lambda: desfazer(tarefas, tarefas_refazer),
+        'refazer': lambda: refazer(tarefas, tarefas_refazer),
+        'clear': lambda: os.system('clear'),
+        'adicionar': lambda: adicionar(tarefa, tarefas),
+        'sair': sair,
+    }
+    comando = comandos.get(tarefa) if comandos.get(tarefa) is not None else \
+        comandos['adicionar']
+    comando()
+    salvar(tarefas, CAMINHO_ARQUIVO)
 
-    elif tarefa == 'D': # Desfazer a última tarefa
-        desfazer(tarefas, tarefaz_refazer)
-        listar(tarefas)
-        continue
 
-    elif tarefa == 'R': # Refazer a última tarefa desfeita
-        refazer(tarefas, tarefaz_refazer)
-        listar(tarefas)
-        continue
-
-    elif tarefa == 'cls': # Limpar a tela
-        os.system( 'cls' )
-        continue
-
-    elif tarefa == 'S': # Sair do programa
-        break
-
-    else: # Adicionar uma nova tarefa
-        adicionar(tarefa, tarefas)
-        listar(tarefas)
-        continue
-    
-    
-
-    
-    
- 
-
-   
+#    if tarefa == 'L': # Listar as tarefas
+#        listar(tarefas) 
+#        continue # Volta para o início do loop
+#    elif tarefa == 'D': # Desfazer a última tarefa
+#        desfazer(tarefas, tarefaz_refazer)
+#        listar(tarefas)
+#        continue
+#    elif tarefa == 'R': # Refazer a última tarefa desfeita
+#        refazer(tarefas, tarefaz_refazer)
+#        listar(tarefas)
+#        continue
+#    elif tarefa == 'cls': # Limpar a tela
+#        os.system( 'cls' )
+#        continue
+#    elif tarefa == 'S': # Sair do programa
+#        break
+#    else: # Adicionar uma nova tarefa
+#        adicionar(tarefa, tarefas)
+#        listar(tarefas)
+#        continue
--- aulas/Intermediario/exercicios/exercicio_119.py
+++ aulas/Intermediario/exercicios/exercicio_119.py
@@ -1,3 +1,4 @@
+import json
 # Exercício - Lista de tarefas com desfazer e refazer
 
 # Funções para manipular a lista de tarefas
@@ -23,6 +24,7 @@ def desfazer(tarefas, tarefas_refazer):
     print ( f'{tarefa=} removida da lista.' )
     tarefas_refazer.append(tarefa) # Adiciona a tarefa removida na lista de refazer
     print (  )
+    listar(tarefas)
  
 # Função para refazer a última tarefa desfeita
 def refazer(tarefas, tarefas_refazer):
@@ -35,6 +37,7 @@ def refazer(tarefas, tarefas_refazer):
     print ( f'{tarefa=} readicionada na lista.' )
     tarefas.append(tarefa) # Adiciona a tarefa removida na lista de tarefas
     print (  )
+    listar(tarefas)
 
 # Função para adicionar uma nova tarefa
 def adicionar(tarefa, tarefas):
@@ -47,10 +50,37 @@ def adicionar(tarefa, tarefas):
     
     tarefas.append(tarefa) # Adiciona a tarefa na lista de tarefas
     print (  )
+    listar(tarefas)
+
+# Função para salvar as tarefas em um arquivo JSON
+def ler(tarefas, caminho_arquivo):
+    dados = []
+    try:
+        with open(caminho_arquivo, 'r', encoding='utf8') as arquivo:
+            dados = json.load(arquivo)
+    except FileNotFoundError:
+        print('Arquivo não existe')
+        salvar(tarefas, caminho_arquivo)
+    return dados
+
+# Função para salvar as tarefas em um arquivo JSON
+def salvar(tarefas, caminho_arquivo):
+    dados = tarefas
+    with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+        dados = json.dump(tarefas, arquivo, indent=2, ensure_ascii=False)
+    return dados
+
+#função para sair
+def sair():
+    print('Saindo...')
+    exit()
+
+
+# Caminho do arquivo JSON para salvar as tarefas
+CAMINHO_ARQUIVO = 'aula119.json'
 
 # Listas para armazenar as tarefas e as tarefas desfeitas
-tarefas = []
+tarefas = ler([], CAMINHO_ARQUIVO)
 
 # Lista para armazenar as tarefas desfeitas para refazer
-tarefaz_refazer = [] 
-
+tarefas_refazer = [] 
