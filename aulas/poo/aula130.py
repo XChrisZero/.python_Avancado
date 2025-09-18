8eafbc5297748aa02fb73df764089db67ee0afa5
XChrisZero <vdevendetal5l@gmail.com>
2025-09-17 21:05:27 -0300

    method vs @classmethod vs @staticmethod

---
+++ aulas/poo/aula130.py
@@ -0,0 +1,41 @@
+# method - self, método de instância
+# @classmethod - cls, método de classe
+# @staticmethod - método estático (❌self, ❌cls)
+#toda vez que você não precisa de self esse metodo é de instancia!
+
+
+class Connection:
+    def __init__(self, host='localhost'): #método de instância
+        self.host = host
+        self.user = None
+        self.password = None
+
+    def set_user(self, user): #método de instância
+        self.user = user
+
+    def set_password(self, password): #método de instância
+        self.password = password
+
+    @classmethod
+    def create_with_auth(cls, user, password): #método de classe
+        connection = cls()
+        connection.user = user
+        connection.password = password
+        return connection
+
+    @staticmethod
+    def log(msg): #método estático
+        print('LOG:', msg)
+
+
+def connection_log(msg):
+    print('LOG:', msg)
+
+
+# c1 = Connection()
+c1 = Connection.create_with_auth('luiz', '1234')
+# c1.set_user('luiz')
+# c1.set_password('123')
+print(Connection.log('Essa é a mensagem de log'))
+print(c1.user)
+print(c1.password)
