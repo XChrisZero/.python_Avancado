322b0368230356e4efc6be5a30c1bc20c3bee4f0
XChrisZero <vdevendetal5l@gmail.com>
2025-10-12 21:22:34 -0300

    projeto calcular data de parcelas

---
+++ aulas/modulos/aula165.py
@@ -0,0 +1,18 @@
+# Exercício solucionado: calculando as datas e parcelas de um empréstimo
+# Maria pegou um empréstimo de 1.000.000
+# para realizar o pagamento em 5 anos.
+# A data em que ela pegou o empréstimo foi
+# 20/12/2020 e o vencimento de cada parcela
+# é no dia 20 de cada mês.
+# - Crie a data do empréstimo
+# - Crie a data do final do empréstimo
+# - Mostre todas as datas de vencimento e o valor de cada parcela
+from exercicios import valor_total, data_emprestimo, delta_anos, data_final, data_parcelas, numero_parcelas, valor_parcela
+    
+print()
+print(
+    f'Você pegou R$ {valor_total:,.2f} para pagar '
+    f'em {delta_anos.years} anos '
+    f'({numero_parcelas} meses) em parcelas de '
+    f'R$ {valor_parcela:,.2f}.'
+)
---
+++ aulas/modulos/exercicios/__init__.py
@@ -0,0 +1,6 @@
+# __init__
+# Este arquivo torna o diretório 'exercicios' um pacote Python.
+# Ele pode ser usado para inicializar o pacote ou definir o que é exportado quando o pacote é importado.
+
+from .exercicio165 import valor_total, data_emprestimo, delta_anos, data_final, data_parcelas, numero_parcelas, valor_parcela
+
