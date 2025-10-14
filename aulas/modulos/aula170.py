80e3b6548c580cc6c629f94672a0f133b3b4dbe5
XChrisZero <vdevendetal5l@gmail.com>
2025-10-13 21:35:46 -0300

    Lib os.listdir

--- aulas/modulos/aula170.py
+++ aulas/modulos/aula170.py
@@ -4,14 +4,15 @@
 # caminho = r'C:\\Users\\luizotavio\\Desktop\\EXEMPLO'
 import os
 
-caminho = os.path.join('/Users', 'luizotavio', 'Desktop', 'EXEMPLO')
+caminho = os.path.join('/Users', 'luizotavio', 'Desktop', 'EXEMPLO') # join monta o caminho correto para o sistema operacional
+# caminho = os.path.join('C:\\', '
 
-for pasta in os.listdir(caminho):
+for pasta in os.listdir(caminho): # lista todas as pastas e arquivos do caminho
     caminho_completo_pasta = os.path.join(caminho, pasta)
     print(pasta)
 
-    if not os.path.isdir(caminho_completo_pasta):
+    if not os.path.isdir(caminho_completo_pasta): # se não for um diretório,
         continue
 
-    for imagem in os.listdir(caminho_completo_pasta):
+    for imagem in os.listdir(caminho_completo_pasta): # lista todas as pastas e arquivos do caminho
         print('  ', imagem)
